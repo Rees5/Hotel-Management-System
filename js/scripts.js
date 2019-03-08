7b9d6399040cea50b653a6f82e6d8308d4fcf4b7
Rees Alumasa <reesalumasa@gmail.com>
2019-03-08 07:16:26 +0300

    Add functionality to book file

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,11 @@ $(document).ready(function() {
     var f = document.getElementById('guest');
     var guest = f.options[f.selectedIndex].value;
     var price = (room * guest);
+    var pay = $("input:radio[name=a]:checked").val();
     $("#total").text(price);
     $("#pay").click(function(event){
       event.preventDefault();
-      var pin = parseInt(prompt("Input your pin"));
+      var pin = parseInt(prompt("Input your "  + pay + "  pin"));
       if(pin>=1){
       alert("Confirmed! You have succssefully paid " + price + " to belmond hotel.");
     } else {
